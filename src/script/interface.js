f4af048eb21f6ebd1cd8d4506a3f14d278951c38
Developer9719 <justin.bat19@gmail.com>
2025-09-21 21:56:33 -0400

    Step 2 of Assignment done

--- src/script/interface.js
+++ src/script/interface.js
@@ -1,5 +1,7 @@
+import '../styles/contentLayout.css';
+
 // Displaying content
-export function display(parentElement = 'body', newElement = 'p', content = 'New content', classes = []) {
+export function display(parentElement = 'body', newElement = 'p', content, classes = []) {
     const parent = document.querySelector(parentElement);
 
     if (!parent) {
@@ -8,7 +10,9 @@ export function display(parentElement = 'body', newElement = 'p', content = 'New
     }
 
     const child = document.createElement(newElement);
-    child.textContent = content;
+    if (content != undefined) {
+        child.textContent = content;
+    }    
 
     // Add classes using forEach and the classList property
     if (Array.isArray(classes) && classes.length > 0) {
@@ -24,4 +28,19 @@ export function display(parentElement = 'body', newElement = 'p', content = 'New
 // Visual design of the site
 export function createLayout() {
     // Inspo: https://bordio.com/wp-content/themes/understrap/images/to-do-list/to-do-list-board-xl-1x.webp
+
+    let classList = ['content'];
+    display('main', 'div', undefined, classList);
+    // Left 20% of Page
+    classList = ['leftSide']
+    display('.content', 'div', undefined, classList);
+    display('.leftSide', 'h2', 'Project List', undefined);
+
+    // Right 80% of Page
+    classList = ['rightSide'];
+    display('.content', 'div', undefined, classList);
+    display('.rightSide', 'h2', 'To-Do', undefined);
+
+    // Footer
+    display('footer', 'p', '&copy 2025 To Do App', classList);
 }
