79c1914dc665a922bd2dc7d6295a4fcf951525df
Developer9719 <justin.bat19@gmail.com>
2025-09-21 22:20:37 -0400

    Added pseudocode to the content script page

--- src/script/content.js
+++ src/script/content.js
@@ -1,4 +1,12 @@
 import { display } from "./interface";
+import './todoItemLogic.js';
 
-// Projects on left side bar
-// To-Do items for each project on right side of projects list
+/* Projects on left side bar */
+// Create a button to add a new project
+
+// Display all projects. Use ID's to change view on the right side to dynamically view todos for the specific project
+
+/* To-Do items for each project on right side of projects list */
+// Create a button to add a new task in this project
+
+// Display all task in this project 
