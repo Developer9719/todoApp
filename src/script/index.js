224fe1c131eafee6aeca184e50bc105596483147
Developer9719 <justin.bat19@gmail.com>
2025-09-22 22:03:54 -0400

    Sets up pageLoad module with IIFE opened

--- src/script/index.js
+++ src/script/index.js
@@ -1,6 +1,7 @@
-import './structure.js';
+import './pageLoad.js'; // Runs the IIFE
+import './structure.js'; // Setups up the basic structure
 import  { createLayout, interaction } from './interface.js';
 
 // Called after structure is loaded 
-createLayout();
-interaction();
+createLayout(); // Create the customized layout
+interaction(); // Add user interaction 
---
+++ src/script/pageLoad.js
@@ -0,0 +1,4 @@
+(function () {
+    // Find all current projects and there to-do's stored in localStorage (JS Property)
+    
+})();
